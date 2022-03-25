4c88c9df7fe776413ed2affdb014d1be7133e977
Real Inspiration <ilerwatson22@gmail.com>
2022-03-25 21:08:43 +0000

    Thought Model

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+const Reaction = require('./Reaction');
+var dayjs = require('dayjs');
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: 'Please leave a thought',
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) =>
+        dayjs(createdAtVal).format(' DD MMMM YYYY [at] hh:mm a'),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: { getters: true },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+console.table(get);
+
+module.exports = Thought;
