52c8dec5a1e6cb3aa0ff8682f00d2205f280428a
Real Inspiration <ilerwatson22@gmail.com>
2022-03-28 18:41:23 +0100

    Route to add a friend by id

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,12 +5,16 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
 } = require('../../controllers/userController');
 
 // get and create user routes
 router.route('/').get(getUsers).post(createUser);
 
-/*routes to get single user by id, update single user by id and delete single user by id*/
+/* routes to get single user by id, update single user by id and delete single user by id*/
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// route to add friend by id
+router.route('/:userId/friends/:friendId').post(addFriend);
 
 module.exports = router;
