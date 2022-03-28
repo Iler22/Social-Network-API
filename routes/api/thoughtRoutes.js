4251ec4f7aa0838be9412917243451ec02f36f14
Real Inspiration <ilerwatson22@gmail.com>
2022-03-28 22:54:23 +0100

    Singe thought route added

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 const {
   getThoughts,
   createThought,
+  getSingleThought,
 } = require('../../controllers/thoughtController');
 
 // route to get all thoughts and create a thought
 router.route('/').get(getThoughts).post(createThought);
+
+// route to get a single thought
+router.route('/:thoughtId').get(getSingleThought);
 
 module.exports = router;
