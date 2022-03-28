587157e225b4e993f58b10e6b8026eab3482a1e4
Real Inspiration <ilerwatson22@gmail.com>
2022-03-28 18:15:37 +0100

    Created route to get a user by id

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
-const { getUsers, createUser } = require('../../controllers/userController');
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+} = require('../../controllers/userController');
 
 // get and create user routes
 router.route('/').get(getUsers).post(createUser);
+
+//route to get single user by id
+router.route('/:userId').get(getSingleUser);
 
 module.exports = router;
