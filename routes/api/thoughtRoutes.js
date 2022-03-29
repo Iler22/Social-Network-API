0198798e8a2006f6d33a5798b6405676fff67ce0
Real Inspiration <ilerwatson22@gmail.com>
2022-03-29 23:00:09 +0100

    Added route to update a thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -3,12 +3,13 @@ const {
   getThoughts,
   createThought,
   getSingleThought,
+  updateThought,
 } = require('../../controllers/thoughtController');
 
 // route to get all thoughts and create a thought
 router.route('/').get(getThoughts).post(createThought);
 
-// route to get a single thought
-router.route('/:thoughtId').get(getSingleThought);
+// routes to get a single thought, update a thought
+router.route('/:thoughtId').get(getSingleThought).put(updateThought);
 
 module.exports = router;
